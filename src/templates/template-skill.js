39ab655f0dac560fd2762aae841414cbc040a069
maliciacanv <cantenovictoriomaruja@gmail.com>
2019-05-19 17:50:09 -0500

    agregando imagenes iconos en skill

--- src/templates/template-skill.js
+++ src/templates/template-skill.js
@@ -4,27 +4,26 @@ export const skills = () => {
           <div class="skill">
           <h1 class="h1-skill">SKILL SET</h1>
           <ul class="list"> 
-            <li><i class="fab fa-angular"></i> Framework Angular</li>
-            <li><i class="fab fa-js"></i> JavaScript</li>
-            <li><i class="fab fa-node"></i> Node js</li>
-            <li><i class="fab fa-html5"></i> HTML 5 </li>
-            <li><i class="fab fa-css3"></i> CSS 3 </li>
-            <li><i class="fab fa-sass"></i> Procesador/ Sass</li>
-            <li><i class="fas fa-bold"></i> Bootstrap 4</li>
-            <li><i class="fas fa-code-branch"></i> Librería Fetch</li>
-            <li><i class="fas fa-file-code"></i> HTTP JSON</li>
-            <li><i class="fas fa-database"></i> Firebase Authentication, Cloud Firestore, Storage</li>
-            <li><i class="fas fa-table"></i> Metodología agíl / Scrum - kanban</li>
+            <li><img src="../imagenes/favicon.ico" /> Framework Angular 7</li>
+            <li><img src="../imagenes/typescript.png" /> TypeScript</li>
+            <li><img src="../imagenes/javascript.png" /> JavaScript</li>
+            <li><img src="../imagenes/nodejs.png" /> Node js</li>
+            <li><img src="../imagenes/html-5.png" /> HTML 5 </li>
+            <li><img src="../imagenes/css-3.png" /> CSS 3 </li>
+            <li><img src="../imagenes/sass.png" /> Procesador/ Sass</li>
+            <li><img src="../imagenes/bootstrap.png" /> Bootstrap 4</li>
+            <li><img src="../imagenes/compartir.png" /> Librería Fetch</li>
+            <li><img src="../imagenes/base-de-datos.png" /> Firebase Authentication, Cloud Firestore, Storage</li>
+            <li><img src="../imagenes/pizarra.png" /> Metodología agíl / Scrum - Tablero kanban</li>
           </ul>
           </div>
           <div class="skill">
           <h1 class="h1-skill">TOOLS</h1>
           <ul class="list">
-            <li><i class="fab fa-git"></i> Git</li>
-            <li><i class="fab fa-github"></i> Git Hub</li>
-            <li><i class="fab fa-adobe"></i> Adobe Ilustrator</li>
-            <li><i class="fab fa-adobe"></i> Adobe Indesing</li>
-            <li><i class="fas fa-adjust"></i> Editor de diseño web Figma</li>
+            <li><img src="../imagenes/github.png" /> Git</li>
+            <li><img src="../imagenes/logo-git.png" /> Git Hub</li>
+            <li><img src="../imagenes/ilustrador.png" /> Adobe Ilustrator</li>
+            <li><img src="../imagenes/imagen.png" /> Editor de diseño web Figma</li>
           </ul>
           </div>
          </div>`;
